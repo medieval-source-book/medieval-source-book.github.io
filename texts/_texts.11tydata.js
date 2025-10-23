cd56a52dbb5da1c921cdea382469a1e04f13ae41
Quinn Dombrowski <qad@stanford.edu>
2025-10-22 23:24:53 -0700

    path fix

--- texts/_texts.11tydata.js
+++ texts/_texts.11tydata.js
@@ -3,9 +3,16 @@ module.exports = {
     permalink: (data) => {
       // If there's already a permalink and it doesn't end with / or .html, add .html
       if (data.permalink && typeof data.permalink === 'string') {
-        if (!data.permalink.endsWith('/') && !data.permalink.endsWith('.html')) {
-          return data.permalink + '.html';
+        let permalink = data.permalink;
+        // Ensure it starts with /
+        if (!permalink.startsWith('/')) {
+          permalink = '/' + permalink;
         }
+        // Add .html if it doesn't end with / or .html
+        if (!permalink.endsWith('/') && !permalink.endsWith('.html')) {
+          permalink = permalink + '.html';
+        }
+        return permalink;
       }
       return data.permalink;
     }
